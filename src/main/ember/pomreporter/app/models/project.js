c03bc8d25c8d0beca2d6e005f1d2f4c8c3299613
Jason Davis <jdavis@openactive.org>
2017-01-29 16:25:05 -0700

    Removing ember and katharsis

--- src/main/ember/pomreporter/app/models/project.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  path: DS.attr("string"),
-  xpathExpression: DS.attr("string"),
-  name: DS.attr("name")
-});
--- src/main/java/org/openactive/PomReporter/katharsis/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- *  http://katharsis-jsonapi.readthedocs.io/en/latest/user-docs.html
- *
- * Created by mohadib on 1/23/17.
- */
-package org.openactive.PomReporter.katharsis;
