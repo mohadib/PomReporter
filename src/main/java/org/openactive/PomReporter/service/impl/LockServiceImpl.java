0085d9a2f216c4a127c0acaee327e2cd51d3569a
Jason Davis <jdavis@openactive.org>
2017-02-06 07:26:44 -0700

    comment

--- src/main/java/org/openactive/PomReporter/service/impl/LockServiceImpl.java
+++ src/main/java/org/openactive/PomReporter/service/impl/LockServiceImpl.java
@@ -7,14 +7,19 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by jdavis on 1/25/17.
+ *
+ *  An application wide lock to keep
+ *  deleting of projects, creds, projectgroups from
+ *  happening while ProjectUpdateService is running.
+ *
  */
 @Service
 public class LockServiceImpl
 {
-	private final ReentrantLock pomSerivceRunLock = new ReentrantLock( true );
+  private final ReentrantLock pomSerivceRunLock = new ReentrantLock(true);
 
-	public Lock getPomServiceRunLock()
-	{
-		return pomSerivceRunLock;
-	}
+  public Lock getPomServiceRunLock()
+  {
+    return pomSerivceRunLock;
+  }
 }
